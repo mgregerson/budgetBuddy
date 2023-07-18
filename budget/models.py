131c0c7a6f3f3e2b54b705183116c21938ce2a5c
mgregerson <mgregerson4321@gmail.com>
2023-07-18 11:20:39 -0400

    Added new model -> Income
    Fields:
    Date
    Description
    Source
    Amount
    Foreign Key Relationship to a project

--- budget/models.py
+++ budget/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.text import slugify
+from .choices import INCOME_FREQUENCY_CHOICES
 
 class Project(models.Model):
     name = models.CharField(max_length=100)
@@ -12,10 +13,15 @@ class Project(models.Model):
 
     def budget_left(self):
         expense_list = Expense.objects.filter(project=self)
+        income_list = Income.objects.filter(project=self)
+
         total_expense_amount = 0
 
         for expense in expense_list:
             total_expense_amount += expense.amount
+
+        for income in income_list:
+            total_expense_amount -= income.amount
 
         return self.budget - total_expense_amount
 
@@ -32,6 +38,16 @@ class Expense(models.Model):
     title = models.CharField(max_length = 100)
     amount = models.DecimalField(max_digits=8, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('-amount',)
+
+class Income(models.Model):
+    date = models.DateField()
+    description = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    source = models.CharField(max_length=255)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='income_sources')
 
     class Meta:
         ordering = ('-amount',)
