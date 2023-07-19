dbcd72de8a848b4aa551d5bf7c37d2c209e6840c
mgregerson <mgregerson4321@gmail.com>
2023-07-19 14:18:40 -0400

    Added new Model: Account ->
    name = models.CharField(max_length=100)
        slug = models.SlugField(max_length=100)
        balance = models.DecimalField(max_digits=10, decimal_places=2)
        currency = models.CharField(max_length=3, default=USD, choices=CURRENCY_CHOICES)
        account_number = models.CharField(max_length=50, blank=True)
        routing_number = models.CharField(max_length=50, blank=True)
        institution = models.CharField(max_length=100, blank=True)
        description = models.TextField(blank=True)

--- budget/models.py
+++ budget/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils.text import slugify
-from .choices import INCOME_FREQUENCY_CHOICES
+from .choices import INCOME_FREQUENCY_CHOICES, CURRENCY_CHOICES
 
 class Project(models.Model):
     name = models.CharField(max_length=100)
@@ -29,13 +29,32 @@ class Project(models.Model):
         expense_list = Expense.objects.filter(project=self)
         income_list = Income.objects.filter(project=self)
         return len(expense_list) + len(income_list)
+    
+class Account(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
+    currency = models.CharField(max_length=3, default='USD', choices=CURRENCY_CHOICES)
+    account_number = models.CharField(max_length=50, blank=True)
+    routing_number = models.CharField(max_length=50, blank=True)
+    institution = models.CharField(max_length=100, blank=True)
+    description = models.TextField(blank=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Account, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
 
 class Category(models.Model):
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, null=True)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=50)
-
+    
 class Expense(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='expenses')
+    account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='expenses', null=True)
     title = models.CharField(max_length = 100)
     amount = models.DecimalField(max_digits=8, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
@@ -43,13 +62,24 @@ class Expense(models.Model):
 
     class Meta:
         ordering = ('-amount',)
-
+    
 class Income(models.Model):
     date = models.DateField()
     description = models.CharField(max_length=255)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     source = models.CharField(max_length=255)
-    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='income_sources')
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='income_sources', null=True)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='income_sources', null=True)
 
     class Meta:
         ordering = ('-amount',)
+
+# class Goal(models.Model):
+#     account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name="goals")
+#     end_date = models.DateField()
+#     target_amount = models.DecimalField(max_digits=10, decimal_places=2)
+#     current_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+#     achieved = models.BooleanField(default=False)
+
+    
+    
