c381896e01f83e127f7e28a4f7d380c04cd1219a
mgregerson <mgregerson4321@gmail.com>
2023-07-19 16:40:11 -0400

    Set CustomUser model to AUTH_USER_MODEL

--- budgetBuddy/settings.py
+++ budgetBuddy/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'budget',
+    'user',
     'bootstrap5',
 ]
 
@@ -91,6 +92,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'user.CustomUser'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
