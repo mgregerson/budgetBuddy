49007d3dc1c04effcdd90ea8edb87878e3344bd8
mgregerson <mgregerson4321@gmail.com>
2023-07-19 14:23:27 -0400

    Added 20 most common currencies worldwide to list of available currencies

---
+++ budget/choices.py
@@ -0,0 +1,33 @@
+INCOME_FREQUENCY_CHOICES = [
+    ('one-time', 'One-time'),
+    ('daily', 'Daily'),
+    ('weekly', 'Weekly'),
+    ('bi-weekly', 'Bi-Weekly'),
+    ('monthly', 'Monthly'),
+    ('quarterly', 'Quarterly'),
+    ('bi-annually', 'Bi-Annually'),
+    ('annually', 'Annually'),
+]
+
+CURRENCY_CHOICES = [
+    ('USD', 'United States Dollar'),
+    ('EUR', 'Euro'),
+    ('JPY', 'Japanese Yen'),
+    ('GBP', 'British Pound Sterling'),
+    ('AUD', 'Australian Dollar'),
+    ('CAD', 'Canadian Dollar'),
+    ('CHF', 'Swiss Franc'),
+    ('CNY', 'Chinese Yuan'),
+    ('SEK', 'Swedish Krona'),
+    ('NZD', 'New Zealand Dollar'),
+    ('MXN', 'Mexican Peso'),
+    ('SGD', 'Singapore Dollar'),
+    ('HKD', 'Hong Kong Dollar'),
+    ('NOK', 'Norwegian Krone'),
+    ('KRW', 'South Korean Won'),
+    ('TRY', 'Turkish Lira'),
+    ('RUB', 'Russian Ruble'),
+    ('INR', 'Indian Rupee'),
+    ('BRL', 'Brazilian Real'),
+    ('ZAR', 'South African Rand'),
+]
