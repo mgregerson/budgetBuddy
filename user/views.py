4d3d3d9d75847e0de2a10735ae1c993fe5591261
mgregerson <mgregerson4321@gmail.com>
2023-07-19 18:01:07 -0400

    Added new views for the following: quote> quote> - register_request: Displays register form for a new user. Handles register, redirects to user profile on success. quote> - login_request: Displays login form for an existing user. Handles login, redirects to user profile on success. quote> - user_profile: Displays users profile page.

--- user/views.py
+++ user/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import  render, redirect
+from .forms import CustomUserCreationForm, CustomUserChangeForm, CustomUserLoginForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-# Create your views here.
+def register_request(request):
+	if request.method == "POST":
+		form = CustomUserCreationForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
+			messages.success(request, "Registration successful." )
+			return redirect('profile')
+		messages.error(request, "Unsuccessful registration. Invalid information.")
+	form = CustomUserCreationForm()
+	return render (request=request, template_name="auth/register.html", context={"register_form":form})
+
+def login_request(request):
+    if request.method == "POST":
+        form = CustomUserLoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('profile')
+            else:
+                form.add_error(None, 'Invalid username or password.')
+    else:
+        form = CustomUserLoginForm()
+    return render(request, 'accounts/login.html', {'form': form})
+
+@login_required
+def user_profile(request):
+    user = request.user
+    context = {
+        'user': user
+    }
+    return render(request, 'auth/profile.html', context)
