d4edc27bfc6d677f20a7b451ca7afe49637d057f
mgregerson <mgregerson4321@gmail.com>
2023-07-19 17:58:32 -0400

    Added new urls to /login, /register, and /profile for user class

--- user/urls.py
+++ user/urls.py
@@ -19,5 +19,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path("register/", views.register_request, name="register"),
+    path("login/", views.login_request, name="login"),
+    path('profile/', views.user_profile, name="profile")
 ]
