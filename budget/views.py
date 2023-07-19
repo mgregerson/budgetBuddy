a2893dd721b38a7ddbca1c6687aebc7113ecc140
mgregerson <mgregerson4321@gmail.com>
2023-07-19 14:22:54 -0400

    Added new view functions ->
    account_list: Renders account-list.html with a list of all accounts
    account_detail: Renders account-detail.html. Handles conditional logic for adding a new expense or source of income to the account

--- budget/views.py
+++ budget/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.views.generic import CreateView
 from django.utils.text import slugify
 
-from .models import Project, Category, Expense, Income
+from .models import Project, Category, Expense, Income, Account
 from .forms import ExpenseForm, IncomeForm
 
 import json
@@ -12,6 +12,70 @@ def project_list(request):
     project_list = Project.objects.all()
     
     return render(request, 'budget/project-list.html', {'project_list': project_list})
+
+def account_list(request):
+    account_list = Account.objects.all()
+
+    return render(request, 'account/account-list.html', {'account_list': account_list})
+
+def account_detail(request, account_slug):
+    account = get_object_or_404(Account, slug=account_slug)
+    expense_form = ExpenseForm()
+    income_form = IncomeForm()
+    
+    if request.method == 'GET':
+        return render(request, 'account/account-detail.html', {
+            'account': account,
+            'expense_list': account.expenses.all(),
+            'income_list': account.income_sources.all()
+        })
+    elif request.method == 'POST':
+        if 'expense_submit' in request.POST:
+            expense_form = ExpenseForm(request.POST)
+            if expense_form.is_valid():
+                date = expense_form.cleaned_data['date']
+                title = expense_form.cleaned_data['title']
+                amount = expense_form.cleaned_data['amount']
+                category_name = expense_form.cleaned_data['category']
+
+                category = get_object_or_404(Category, account=account, name=category_name)
+
+                Expense.objects.create(
+                  account=account,
+                  date=date,
+                  title=title,
+                  amount=amount,
+                  category=category
+              ).save()
+    elif 'income_submit' in request.POST:
+          income_form = IncomeForm(request.POST)
+          if income_form.is_valid():
+              date = income_form.cleaned_data['date']
+              description = income_form.cleaned_data['description']
+              amount = income_form.cleaned_data['amount']
+              source = income_form.cleaned_data['source']
+
+              Income.objects.create(
+                  account=account,
+                  date=date,
+                  description=description,
+                  amount=amount,
+                  source=source,
+              ).save()
+    elif request.method == 'DELETE':
+        data = json.loads(request.body)
+        item_id = data['id']
+        item_type = data['type']
+
+        if item_type == 'expense':
+            expense = get_object_or_404(Expense, id=item_id)
+            expense.delete()
+        elif item_type == 'income':
+            income = get_object_or_404(Income, id=item_id)
+            income.delete()
+        return HttpResponse('')
+
+    return HttpResponseRedirect(account_slug)
 
 def project_detail(request, project_slug):
     project = get_object_or_404(Project, slug=project_slug)
@@ -95,6 +159,20 @@ class ProjectCreateView(CreateView):
             ).save()
 
         return HttpResponseRedirect(self.get_success_url())
+    
+    def get_success_url(self):
+        return slugify(self.request.POST['name'])
+    
+class AccountCreateView(CreateView):
+    model = Account
+    template_name = 'account/add-account.html'
+    fields = ('name', 'balance', 'currency', 'account_number', 'routing_number', 'institution', 'description')
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.save()
+
+        return HttpResponseRedirect(f"accounts/{self.get_success_url()}")
     
     def get_success_url(self):
         return slugify(self.request.POST['name'])
