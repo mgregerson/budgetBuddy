6013878f7dc62321b5164d55a182288e1906118b
mgregerson <mgregerson4321@gmail.com>
2023-07-20 13:45:04 -0400

    Added accounts url routing functionality

--- budgetBuddy/urls.py
+++ budgetBuddy/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('budget.urls')),
     path('user/', include('user.urls')),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),  # Include AllAuth URLs here
 ]
